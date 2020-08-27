e31ae9dd1216d9e09e8ff724b338f6dab97a0a5c
Gibran Rodriguez <brangi000@gmail.com>
2020-08-26 22:22:56 -0500

    conditional for testing run

--- lib/news/jobs/init.ex
+++ lib/news/jobs/init.ex
@@ -2,13 +2,19 @@ defmodule News.Jobs.Init do
   alias News.Jobs.Fetcher
   @moduledoc false
   def all_categories do
-    {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27017/news")
-    ensure_uniq(conn)
-    cursor = Mongo.find(conn, "site", %{})
-    c = cursor |> Enum.to_list()
-    [categories_queries] = c
-    Enum.map categories_queries["categories"], fn(c) ->
-      Supervisor.child_spec({Fetcher, c}, id: String.to_atom(c))
+    if Application.fetch_env!(:news, :app_env) === "staging" do
+      Enum.map ["nfl","nba","mls","soccer"], fn(c) ->
+        Supervisor.child_spec({Fetcher, c}, id: String.to_atom(c))
+      end
+    else
+      {:ok, conn} = Mongo.start_link(url: "mongodb://localhost:27017/news")
+      ensure_uniq(conn)
+      cursor = Mongo.find(conn, "site", %{})
+      c = cursor |> Enum.to_list()
+      [categories_queries] = c
+      Enum.map categories_queries["categories"], fn(c) ->
+        Supervisor.child_spec({Fetcher, c}, id: String.to_atom(c))
+      end
     end
   end
   defp ensure_uniq(conn) do
